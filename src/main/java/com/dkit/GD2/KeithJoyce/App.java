2a3438972a82ebf362076ebdeaca62d0569ee7f1
KeithJoycemb <keithjoycemb@gmail.com>
2020-12-06 06:24:31 +0000

    Test commit for developing branch
    This is a testing commit to make sure that the developing branch is
    working as intented

--- src/main/java/com/dkit/GD2/KeithJoyce/App.java
+++ src/main/java/com/dkit/GD2/KeithJoyce/App.java
@@ -1,13 +1,13 @@
 package com.dkit.GD2.KeithJoyce;
 
 /**
- * 
+ *
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-
+        System.out.println("TESTING THE DEVELOPING BRANCH FOR GITHUB");
     }
 }
