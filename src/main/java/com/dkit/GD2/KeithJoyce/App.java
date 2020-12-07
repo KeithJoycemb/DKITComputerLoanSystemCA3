55cd1b7f4ee440ac81a73e1012d4e9628cb8983b
KeithJoycemb <keithjoycemb@gmail.com>
2020-12-07 07:41:30 +0000

    pushing final inheritance from computer to RaspberryPi all working fine

--- src/main/java/com/dkit/GD2/KeithJoyce/App.java
+++ src/main/java/com/dkit/GD2/KeithJoyce/App.java
@@ -13,16 +13,23 @@ public class App
         System.out.println(computer);
 
 
-        System.out.println("INHERATANCE OF COMPUTER TO DESKTOP");
+        System.out.println("INHERITANCE OF COMPUTER TO DESKTOP");
 
         Desktop desktop = new Desktop("Dell", "Intel i7-9700k", "32GB", "1TB", "5kg","DKIT-2020", "10/04/2019",1);
         System.out.println(desktop);
 
-        System.out.println("INHERATANCE OF COMPUTER TO LAPTOP");
+        System.out.println("INHERITANCE OF COMPUTER TO LAPTOP");
 
         Laptop laptop = new Laptop("Dell", "Intel i7-9700k", "32GB", "1TB", "5kg","DKIT-2020", "10/04/2019",
                 "18 inches","8 hours");
         System.out.println(laptop);
+
+
+        System.out.println("INHERITANCE OF RASPBERRY PI TO LAPTOP");
+
+        RaspberryPi raspberryPi = new RaspberryPi("Raspberry Pi", "BCM2711", "8GB", "23g", "DKIT-2020", "10/04/2020",
+                "32GB", 40);
+        System.out.println(raspberryPi);
 
 
 
