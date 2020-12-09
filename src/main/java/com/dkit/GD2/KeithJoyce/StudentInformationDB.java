625400ad04de4f3d156af97451eb6926007c4484
KeithJoycemb <keithjoycemb@gmail.com>
2020-12-09 09:47:14 +0000

    Adding save student to student information db class
    added a write student to file to the StudentInformationDB class

--- src/main/java/com/dkit/GD2/KeithJoyce/StudentInformationDB.java
+++ src/main/java/com/dkit/GD2/KeithJoyce/StudentInformationDB.java
@@ -1,7 +1,5 @@
 package com.dkit.GD2.KeithJoyce;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -38,8 +36,28 @@ public class StudentInformationDB
         {
             System.out.println(Colours.RED + "System could not load in student information" + Colours.RESET);
         }
-
     }
+
+    public void saveStudentInformationToFile()
+    {
+        try(BufferedWriter studentsInfoFile = new BufferedWriter(new FileWriter("StudentInformation.txt")))
+        {
+            for(StudentInformation student : studentsInfo)
+            {
+                studentsInfoFile.write(student.getStudentName() + "," + student.getStudentEmail() + "," + student.getStudentID() + "," +
+                        student.getStudentPhoneNumber() + "," + student.getStudentComputersOnLoan());
+
+                studentsInfoFile.write("\n");
+            }
+        }
+        catch(IOException ioe)
+        {
+            System.out.println(Colours.RED+"Sorry could not save student to system"+Colours.RESET);
+        }
+    }
+
+
+
 
 
 }
